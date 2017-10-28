31722b14b2bf24f33b9bf81d7f0bcd10324bd24e
hulb <hulb@live.cn>
2017-10-28 16:51:30 +0800

    init spider for ziroom.com

---
+++ ziroom/items.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ZiroomItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    link = scrapy.Field()
+    floor = scrapy.Field()
+    allfloor = scrapy.Field()
+    town = scrapy.Field()
+    layout = scrapy.Field()
+    nearbymetroline = scrapy.Field()
+    nearbymetrostation = scrapy.Field()
+    nearbymetrodistance = scrapy.Field()
+    price = scrapy.Field()
+    title = scrapy.Field()
+    size = scrapy.Field()
+
+
---
+++ ziroom/spiders/ziroomSpider.py
@@ -0,0 +1,35 @@
+#!-*-coding:utf-8-*
+
+import scrapy
+from scrapy.selector import HtmlXPathSelector
+from ..items import ZiroomItem
+
+class ZiroomSpider(scrapy.Spider):
+    name = 'ziroomFinder'
+
+
+    def start_requests(self):
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+        }
+        urls = ['http://sh.ziroom.com/z/nl/z3-r1-x2-o4.html?']
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse, headers=headers)
+
+    def parse(self, response):
+        for house in response.css('li.clearfix'):
+            room = ZiroomItem()
+            import pdb; pdb.set_trace()
+            town, metro = house.css('h4 a::text').extract_first().split(' ')
+            detail = house.css('div.detail p span').extract()
+            floor = detail[1].css('text').extract_first()
+
+            room['allfloor'] = floor.split('/')[1][0:3]
+            room['floor'] = floor.split('/')[0]
+            room['layout'] = detail[2].css('text').extract_first()
+            room['size'] = detail[0].css('text').extract_first()
+            room['title'] = house.css('h3 a::text').extract_first()
+            room['link'] = house.css('h3 a::attr(href)').extract_first()
+            room['town'] = town[1:-1]
+            room['nearbymetroline'] = metro.split(u'\u53f7\u7ebf')[0] if metro else ''
+            room['nearbymetrostation'] = metro.split(u'\u53f7\u7ebf')[1] if metro else ''
