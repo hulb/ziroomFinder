79c505c05a33c586ddd1c21a40eca7e0dbe38c7c
hulb <hulb@live.cn>
2017-11-14 14:14:03 +0800

    add requirements and schedule job

--- start.py
+++ start.py
@@ -6,15 +6,23 @@ from scrapy.crawler import CrawlerProcess
 from ziroom.spiders.ziroomSpider import ZiroomSpider
 import pymongo
 from ziroom.settings import MONGO_DB, MONGO_URI
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 def crawlWorker():
     process = CrawlerProcess()
     process.crawl(ZiroomSpider)
     process.start()
 
-if __name__ == '__main__':
+def job():
     # start 2 process, one for update room status , one for crawl new rooms
     p = multiprocessing.Process(target=crawlWorker)
     p.start()
+
+if __name__ == '__main__':
+    schedule = BlockingScheduler()
+    schedule.add_job(job, 'interval', days=1, start_date='2017-11-11 4:22:16')
+    schedule.start()
+
+    
 
 
